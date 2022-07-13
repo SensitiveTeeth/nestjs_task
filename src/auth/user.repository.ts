1486d106ad2284cd73abf89a92a6c6d185627cdc
SensitiveTeeth <sem_lal@yahoo.com.hk>
2022-07-13 15:07:22 +0800

    Alter: added jwt module and api

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,15 +1,33 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const task = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
-    await this.save(task);
+    try {
+      await this.save(task);
+    } catch (err) {
+      if (err.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
