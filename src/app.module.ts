dd23402ace59f10c476d6ba20d5ecf0bba7e1c71
SensitiveTeeth <sem_lal@yahoo.com.hk>
2022-07-14 16:51:30 +0800

    Alter: config ssl on nest and add default port: g

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,16 +15,24 @@ import { configValidationSchema } from './config.schema';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        host: configService.get('DB_HOST'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        port: configService.get('DB_PORT'),
-        database: configService.get('DB_USERNAME'),
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') === 'prod';
+
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          port: configService.get('DB_PORT'),
+          database: configService.get('DB_USERNAME'),
+        };
+      },
     }),
     AuthModule,
   ],
